d457daccd08dec9e62be530d1b0487067d84dafa
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-02-03 09:41:54 +0530

    Conversion

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,21 @@
+//let score =33
+//let score ="33abc"
+// let score -=null
+// let score =undefined
+// let score ="Khetesh"
+// let score =true
+ //let score =1
+ // let score =""
+// console.log(typeof (score))
+// console.log(typeof score)
+
+// let valueInNumber =Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// let login =Boolean(score)
+// console.log(login)
+// console.log(typeof login)
+
+// "33abs"=> NaN
+
+
