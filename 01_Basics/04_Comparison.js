5907e6a7b42945fd66cba4256516e35b275a9fa2
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-02-04 22:37:48 +0530

    Intoial commit

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// const { log } = require("console");
+
+ //console.log("2">1)
+// console.log("02">1)
+
+// console.log(null >0 )
+// console.log(null >=0 )
+ // console.log(null ==0 )
+
+ // console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)   /// avoid this conversions
+
+//=== strict check  data type also check krt
+
+console.log("2"==2)
+console.log("2"===2)
