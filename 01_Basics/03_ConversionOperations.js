dbc4a797a5383a362bc50a280dc628ce78078589
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-02-04 22:06:18 +0530

    Intoial commit

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -9,6 +9,8 @@
 // console.log(typeof (score))
 // console.log(typeof score)
 
+const { log } = require("console")
+
 // let valueInNumber =Number (score)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
@@ -18,4 +20,24 @@
 
 // "33abs"=> NaN
 
+//*******************Operations****************** */
 
+let value =3
+let negvalue =-value
+//console.log(negvalue)
+
+let str1 ="Hello"
+let str2 =" Khetesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" +2 +2)
+console.log(2 +2 +" 1")
+
+console.log(true)
+console.log(+true)
+let x=100
+console.log(++x)
+console.log(x++)
