9bb84261119a35a9c436829c999a8f94f7fbc62c
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-02-07 17:50:42 +0530

    Intial Commit

---
+++ 01_Basics/07_nums_and_maths.js
@@ -0,0 +1,34 @@
+/*
+const score=400
+console.log(score)
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toStrin().length)
+console.log(balance.toFixed(4))
+
+const anotherno=1123.8994
+console.log(anotherno.toFixed(2))
+console.log(anotherno.toPrecision(3))
+
+const hundreds=100000000
+console.log(hundreds.toLocaleString('en-In'))
+*/
+//++++++++++++++++++++MATHS+++++++++++++++++++++++
+/*
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(3.6))
+console.log(Math.ceil(3.6))
+console.log(Math.floor(3.6))
+console.log(Math.min(3,5,2,6,7,8,6))
+console.log(Math.max(3,5,2,6,7,8,6))
+*/
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max =20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
