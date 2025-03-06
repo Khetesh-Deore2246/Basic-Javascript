eab5faf4970e4c17e05ed9ebf293e5ba000c6f78
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-03-06 09:28:34 +0530

    Object 3

---
+++ 01_Basics/13_Object_de_structure_and_JSON_API_intro.js
@@ -0,0 +1,9 @@
+const course ={
+    courseInstructor :"KHETESH",
+    courseFees:"999"
+}
+
+//course.courseInstructor
+const{courseInstructor:instructor}=course
+//console.log(courseInstructor)
+console.log(instructor)
